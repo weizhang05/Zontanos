c6832d499703897a12f8ea1e02c556299f4fd872
weizhang05 <chanweizhang05@hotmail.com>
2019-10-12 15:40:06 +0800

    removed unnecessary spacing

--- camera.py
+++ camera.py
@@ -53,7 +53,6 @@ class VideoCamera(object):
 
 		process_this_frame = not process_this_frame
 
-
 		# Display the results
 		for (top, right, bottom, left), name in zip(face_locations, face_names):
 			# Scale back up face locations since the frame we detected in was scaled to 1/4 size
