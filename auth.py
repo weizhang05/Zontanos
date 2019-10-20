37b6e730f6ad0a2ebd04593d3e1a5cb5308eb87a
etjk <edricteojk@hotmail.com>
2019-10-20 17:06:06 +0800

    Re-add Jin code

--- auth.py
+++ auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, redirect, session, url_for, request, flash, escape, Response
+from flask import Blueprint, render_template, redirect, session, url_for, request, flash, escape, Response, Flask
+# from camera import VideoCamera 
+import cv2
+import face_recognition
+import numpy as np
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 from .models import User
@@ -61,3 +65,94 @@ def signup_post():
 def logout():
     logout_user()
     return redirect(url_for('main.index'))
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@auth.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# Import does not work, so this is the best bet
+class VideoCamera(object):
+	def __init__(self):
+		self.video = cv2.VideoCapture(0)
+		
+	def __del__(self):
+		self.video.release()
+
+	def get_frame(self):
+		obama_image = face_recognition.load_image_file("Jin Xing.jpg")
+		obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+		known_face_encodings = [obama_face_encoding]
+		known_face_names = ["Jin Xing","Sarah Cheok"]
+		face_locations = []
+		face_encodings = []
+		face_names = []
+		process_this_frame = True
+		ret, frame = self.video.read()
+
+		# Resize frame of video to 1/4 size for faster face recognition processing
+		small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+		# Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+		rgb_small_frame = small_frame[:, :, ::-1]
+
+		# Only process every other frame of video to save time
+		if process_this_frame:
+			# Find all the faces and face encodings in the current frame of video
+			face_locations = face_recognition.face_locations(rgb_small_frame)
+			face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+			face_names = []
+			for face_encoding in face_encodings:
+				# See if the face is a match for the known face(s)
+				matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+				name = "Unknown"
+
+				# # If a match was found in known_face_encodings, just use the first one.
+				# if True in matches:
+				#     first_match_index = matches.index(True)
+				#     name = known_face_names[first_match_index]
+
+				# Or instead, use the known face with the smallest distance to the new face
+				face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+				best_match_index = np.argmin(face_distances)
+				if matches[best_match_index]:
+					name = known_face_names[best_match_index]
+
+				face_names.append(name)
+
+		process_this_frame = not process_this_frame
+
+
+		# Display the results
+		for (top, right, bottom, left), name in zip(face_locations, face_names):
+			# Scale back up face locations since the frame we detected in was scaled to 1/4 size
+			top *= 4
+			right *= 4
+			bottom *= 4
+			left *= 4
+
+			# Draw a box around the face
+			cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+			# Draw a label with a name below the face
+			cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+			font = cv2.FONT_HERSHEY_DUPLEX
+			cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+		ret, jpeg = cv2.imencode('.jpg', frame)
+		return jpeg.tobytes()
+	
+	def get_frame2(self):
+		success, image = self.video.read()
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+		ret, jpeg = cv2.imencode('.jpg', image)
+		return jpeg.tobytes()
