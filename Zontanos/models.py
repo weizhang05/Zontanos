203dbace49d1f9e402b0b780542c6bfb27ff7ac9
JensonWee <55829267+JensonWee@users.noreply.github.com>
2019-11-13 15:34:35 +0800

    Update

--- Zontanos/models.py
+++ Zontanos/models.py
@@ -2,8 +2,8 @@ from flask_login import UserMixin
 from . import db
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
-    image = db.Column(db.LargeBinary)
+	id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+	email = db.Column(db.String(100), unique=True)
+	password = db.Column(db.String(100))
+	name = db.Column(db.String(1000))
+	facereco = db.Column(db.String(10000))
